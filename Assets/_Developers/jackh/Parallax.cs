70555353a46c5e9fdc3a4e7e74cf71bfd85e2859
Jack HK <jackhughesking@gmail.com>
2022-02-22 11:00:17 +0000

    parallax todo

---
+++ Assets/_Developers/jackh/Parallax.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
