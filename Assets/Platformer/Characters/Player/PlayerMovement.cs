dd6d66c6f8c02e1d851bc7fc21d284924a3f4d7e
Jack HK <jackhughesking@gmail.com>
2022-03-04 12:34:52 +0000

    dash

--- Assets/Platformer/Characters/Player/PlayerMovement.cs
+++ Assets/Platformer/Characters/Player/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 //JackHK
@@ -10,6 +12,10 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed;
     public float runSpeed;
     public float jumpVelocity;
+    public float test;
+
+    private bool isAttacking = false;
+    private Vector2 facingDirection = Vector2.right;
 
     Vector2 entityMove;
     Rigidbody2D entityPhysics;
@@ -28,12 +34,15 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        int runtimeSeconds = runtimeSeconds + 1; //rename
         Move("walk");
+        Attack();
     }
     private void Update()
     {
         entityMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Jump();
+        
     }
 
     // --------------Functions-------------- 
@@ -46,17 +55,25 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void Move(string state)
+    /*
+     * <params>
+     * </params>
+     * 
+     */
+    private void Move(string state) //state determines animations. ex: walking, running, powered-up, etc.
     {
         bool isPoweredUp = false;
+
         //check if looking backwards
         if (Input.GetAxisRaw("Horizontal") <= -1) 
         {
             entityRenderer.flipX = true;
-        }
+            facingDirection = Vector2.left;
+}
         else
         {
             entityRenderer.flipX = false;
+            facingDirection = Vector2.right;
         }
 
         //run animation decided through velocity
@@ -75,13 +92,47 @@ public class PlayerMovement : MonoBehaviour
             entityPhysics.velocity = new Vector2(entityMove.x * runSpeed * Time.deltaTime, entityPhysics.velocity.y); //movement
 
         }
+        else if (isPoweredUp) {
+            state = "power";
+            entityPhysics.velocity = new Vector2(entityMove.x * runSpeed * Time.deltaTime, entityPhysics.velocity.y);
+        }
         else
         {
             state = "walk";
-            entityPhysics.velocity = new Vector2(entityMove.x * walkSpeed * Time.deltaTime, entityPhysics.velocity.y); //movement
+            entityPhysics.velocity = new Vector2(entityMove.x * walkSpeed * Time.deltaTime, entityPhysics.velocity.y);
         }
 
     }
+
+    private void Attack()
+    {
+        //ground attack
+        if (Input.GetButton("Fire1") && !isAttacking && IsGrounded() && entityAnimator.GetInteger("isAttacking") == 0) 
+        {
+            entityAnimator.SetInteger("isAttacking", 1);
+            //entityPhysics.AddForce(facingDirection * test, ForceMode2D.Impulse);
+            isAttacking = true;
+#if UNITY_EDITOR 
+            Debug.Log("Player: Side Attack");
+#endif
+        } 
+        // in-air down attack
+        else if (Input.GetButtonDown("Fire1") && !isAttacking && !IsGrounded() && entityAnimator.GetInteger("isAttacking") == 0)
+        {
+            entityAnimator.SetInteger("isAttacking", 1);
+            isAttacking = true;
+#if UNITY_EDITOR
+            Debug.Log("Player: Air Attack");
+#endif
+        }
+       
+
+        if (isAttacking && entityAnimator.GetInteger("isAttacking") > 0)
+        {
+            isAttacking = false;
+        }
+    }
+   
 
     private bool IsGrounded()
     {
